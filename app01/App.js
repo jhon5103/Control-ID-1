5f11a5735fa2ca7ce0d0256aef4ce61b44fe7b2b
felipesson <felipesilvafranca2001@gmail.com>
2023-08-20 23:56:58 -0300

    Código atualizado, feito a responsabilidade em relação ao tamanho do form login e senha e o tamanho do botão, agora eles ocupam tamanhos diferentes caso seja mobile ou PC

--- app01/App.js
+++ app01/App.js
@@ -1,22 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput,TouchableOpacity, Dimensions, Image } from 'react-native';
+import { StyleSheet, isMobile, Text, View, TextInput,TouchableOpacity, Dimensions, Image } from 'react-native';
 import React, { useState } from 'react';
 
 export default function App() {
   const [login, setLogin] = useState("");
   const [senha, setSenha] = useState("");
-  const exibirAlerta=()=>{
-    window.alert('Bem-vindo!');
-  };
+  const [pressed, setPressed] = useState(false);
+  const isMobile=Dimensions.get('screen').width <768; //Para dimensionar melhor a tela no smartphone
   const [mostrarSenha, setMostrarSenha] = useState(false);
-  const isMobile=Dimensions.get('screen').width <768;//Para dimensionar melhor a tela no smartphone
   const toggleMostrarSenha = () => {
     setMostrarSenha(!mostrarSenha);
   };
+  const exibirAlerta=()=>{
+    window.alert('Bem-vindo!');
+  };
 
   return (
-    <View style={[styles.container0, isMobile && styles.mobileContainer0]}>
-      <Image
+    <View style={[styles.container0, isMobile ? styles.mobileContainer0 : null]}>
+            <Image
        source={require('./imagens/logo.png')} //importando a logo
        style={styles.logo}// setando o css da logo
        resizeMode="contain" // O modo de redimensionamento da imagem
@@ -25,30 +26,38 @@ export default function App() {
 
       <View style={styles.formContainer}>
         <TextInput
-          style={styles.entrada}// get no css da entrada
+          style={[styles.entrada, isMobile ? styles.mobileEntrada : null]}// get no css da entrada
           value={String(login)}// setando o tipo de dado
-          onChangeText={(texto)=>(setLogin(texto))}
+          onChangeText={(texto)=>setLogin(texto)}
           placeholder='Login'
         />
       
         <TextInput
-          style={styles.entrada}// get no css da entrada
+          style={[styles.entrada, isMobile ? styles.mobileEntrada : null]}// get no css da entrada
           value={String(senha)}// setando o tipo de dado
-          onChangeText={(texto)=>(setSenha(texto))}
+          onChangeText={(texto)=>setSenha(texto)}
           placeholder='Senha'
           secureTextEntry={!mostrarSenha}
         />
+        <View style={[styles.senhaMostrarContainer, styles.mostrarSenha, isMobile ? styles.mobileSenhaMostrarContainer : null]}>
         <TouchableOpacity onPress={toggleMostrarSenha}>
-        <Text style={styles.senhaMostrar}>{mostrarSenha ? "Esconder Senha" : "Mostrar Senha"}</Text>
+        <Text style={[styles.mostrarSenha, isMobile ? styles.mobileSenhaMostrarContainer : null]}>{mostrarSenha ? "Esconder Senha" : "Mostrar Senha"}</Text>
         </TouchableOpacity>
+        </View>
+       <TouchableOpacity 
+        onPress={exibirAlerta}
+        onPressIn={()=>setPressed(true)}
+        onPressOut={()=> setPressed(false)}
+        style={[styles.buttonCicle, pressed && styles.buttonPressed,]}>
 
-       <TouchableOpacity onPress={exibirAlerta} style={styles.buttonCicle}>
           <Text style={styles.buttonText}>Acessar</Text>
+          
         </TouchableOpacity>
   
       </View>
       
       <StatusBar style="auto" />
+
     </View>
     
   );
@@ -70,8 +79,10 @@ const styles = StyleSheet.create({
     marginBottom: 30,
   },
   formContainer:{// css do container do conteúdo da página 
-    width: '70%',
-    padding: 30,           
+    padding: 30,
+    alignItems: 'center',
+    justifyContent:'center',
+    width:'40%',
   },
   entrada:{// css da entrada de dados, login e senha
     fontSize:15,
@@ -80,13 +91,16 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
     borderRadius: 8,
+    width:'60%',
+  },
+  mobileEntrada: {
+    width: '250%',
   },
   buttonCicle:{// css do botão
     backgroundColor:'black',
-    padding:3,
-    maxWidth:'50%',
-    marginLeft:'25%',
-    marginTop:'15%',
+    padding:10,
+    alignItems:'center',
+    marginTop:'10%',
     shadowRadius: 5,
     borderRadius: 8,
     shadowColor: '#000',
@@ -104,8 +118,23 @@ const styles = StyleSheet.create({
     height: 200, 
     marginTop: 20, 
   },
-  senhaMostrar:{// css do mostrar senha
+  mostrarSenha:{// css do mostrar senha
     fontSize:12,
     textDecorationLine: 'underline',
+    alignSelf: 'flex-start',
+
   },
+  senhaMostrarContainer: {
+    alignSelf: 'flex-start', // Alinhar o container ao canto esquerdo
+    width:'60%',
+    justifyContent:'center',
+    marginLeft:'20%'
+  },
+  mobileSenhaMostrarContainer:{
+    alignSelf: 'flex-start', // Alinhar o container ao canto esquerdo
+    width:'100%',
+    justifyContent:'center',
+    marginLeft:'-38%'
+  }
+
 });
